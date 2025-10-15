192e8a4e8bb1c520d3f122318672c0dbfd2db477
Smit Patel <psmit9936@gmail.com>
2025-10-15 15:15:26 -0400

    Added stateHook

---
+++ React-Learning/src/stateHook.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function StateHook() {
+    const [count, setCount] = React.useState(0);
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+    return(
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+export default StateHook;
